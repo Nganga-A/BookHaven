3d41f2083dad00f6614e61280e08ae776a778faf
Mwasigwa <gathirimu.mwasigwa@student.moringaschool.com>
2023-10-04 15:58:14 +0300

    Done

---
+++ client/src/Categories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Category = () => {
+  return (
+    <div>
+      <h3>Categories</h3>
+      <ul className="list-group">
+        <li className="list-group-item">Art & Design</li>
+        <li className="list-group-item">Educational</li>
+        <li className="list-group-item">Top Picks</li>
+        <li className="list-group-item">Sci-Fi</li>
+        <li className="list-group-item">Horror</li>
+        <li className="list-group-item">Fiction</li>
+        <li className="list-group-item">Non-Fiction</li>
+      </ul>
+    </div> 
+  );
+};
+
+export default Category;
