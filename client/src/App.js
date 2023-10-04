7a311af5be4d04da5089abef342592b541d7e19a
gift kimani <gift.kimani@students.moringaschool.com>
2023-10-04 13:20:54 +0300

    added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import {BrowserRouter, Routes, Route } from 'react-router-dom '
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './login.css'
 
 function Login() {
   const [loginData, setLoginData] = useState({
@@ -18,25 +19,44 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
+    <div className='login template d-flex justify-content-center align-items-center vh-100 bg-primary'>
+      <div className='form container p-5 rounded bg-white'>
+          <form onSubmit={handleSubmit}>
+      <h1 className='text-center'>Login</h1>
+        <div className='mb-2'>
         <input
           type="email"
           name="email"
-          placeholder="Email"
+          placeholder="Enter Email"
           value={loginData.email}
           onChange={handleChange}
         />
+        </div>
+
+        <div className='mb-2'>
         <input
           type="password"
           name="password"
-          placeholder="Password"
+          placeholder=" Enter Password"
           value={loginData.password}
           onChange={handleChange}
         />
+        </div>
+        <div className='mb-2'>
+        
+            <input type='checkbox' className='custom-control custom-checkbox'id='check'/>
+            <label htmlFor='check' className='custom-input-label ms-2'> Remember me</label>
+            </div>
+
+        <div className='d-grid'>   
         <button type="submit">Login</button>
-      </form>
+        </div> 
+        <p className='text-end mt-2'>
+              forgot <a href=''>Password?</a><link to='/signup'className='ms-2'>Signup</link>
+        </p>
+
+          </form>
+      </div>
     </div>
   );
 }
