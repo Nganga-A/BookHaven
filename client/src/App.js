ba730b2392d7800e32653f2f8c2780cdb284a8d2
Your Name <youremail@example.com>
2023-10-04 21:53:40 +0300

    updated Main.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+
 // App.js
 import './App.css';
 import React from "react";
@@ -9,16 +10,19 @@ import Navbar from './component/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './component/Navbar.css'; 
 import './component/Footer.css'; 
+import Login from './components/Login'
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="">
-      <Router> 
+      <Router>
         {/* <Footer />  */}
-
         <Navbar />
         <Routes>
           <Route path="/home" element={<Main />} />
+          <Route path="/" element={<Login />} /> 
+          <Route path="/Signup" element={<Signup />} /> 
         </Routes>
       </Router>
     </div>
