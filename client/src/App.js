415ab3ad6acdb9eca2b735e05c928b0a4b3cd3df
gift kimani <gift.kimani@students.moringaschool.com>
2023-10-04 12:12:30 +0300

    added some changes in functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Signup from './components/signup';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './components/Signup'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
           <Routes>
-            <Route path= '/signup' element={<signup/>}></Route>
+            <Route path= '/signup' element={<Signup/>}></Route>
           </Routes>
       
       </BrowserRouter>
