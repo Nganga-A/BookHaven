011119e3eb5704792d569531c115c17303692184
Mwasigwa <gathirimu.mwasigwa@student.moringaschool.com>
2023-10-06 00:02:17 +0300

    endpoints updated

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 function Favorites() {
   const [favorites, setFavorites] = useState([]);
+  const [newFavoriteBookId, setNewFavoriteBookId] = useState('');
 
   useEffect(() => {
-    // Fetch the list of favorite books from the Flask API
+    // Fetch the list of favorites when the component mounts
     axios.get('/favorites')
       .then(response => {
         setFavorites(response.data);
@@ -15,13 +16,34 @@ function Favorites() {
       });
   }, []);
 
+  const addToFavorites = () => {
+    const userId = 1; 
+    axios.post('/favorites', { user_id: userId, book_id: newFavoriteBookId })
+      .then(response => {
+        setFavorites([...favorites, response.data]);
+        setNewFavoriteBookId('');
+      })
+      .catch(error => {
+        console.error('Error adding favorite:', error);
+      });
+  };
+
   return (
     <div>
       <h2>My Favorites</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter book ID"
+          value={newFavoriteBookId}
+          onChange={(e) => setNewFavoriteBookId(e.target.value)}
+        />
+        <button onClick={addToFavorites}>Add to Favorites</button>
+      </div>
       <ul>
         {favorites.map(favorite => (
           <li key={favorite.id}>
-            {}
+            Book ID: {favorite.book_id}
           </li>
         ))}
       </ul>
