f206473d8deaab8ddeef264462b96d3222aeaf41
Your Name <youremail@example.com>
2023-10-06 00:54:41 +0300

    updated home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
-// import Header from './components/Header/Header';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header/Header'; // Adjust the path as needed
-
-
+import Header from '../components/Header/Header'; 
+import BookList from '../components/BookList/BookList'; 
 
 const Home = () => {
   return (
     <main>
-        <Header />
-        <Outlet />
+      <Header />
+      <BookList />
+      <Outlet /> 
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
