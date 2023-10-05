cb05837251044048280676120a2c242e91ac884a
Your Name <youremail@example.com>
2023-10-05 23:41:35 +0300

    deleted .eslintrc.cjs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,18 @@
-// src/index.js
+// // src/index.js
+// import React from 'react';
+// import { ReactDOM } from 'react';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 import React from 'react';
-import { ReactDOM } from 'react';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your main App component
 
 ReactDOM.render(
   <React.StrictMode>
