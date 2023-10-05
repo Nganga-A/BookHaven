687231841d003ead68faff44e5a5f8c20c773cb4
mike <mjowiti@gmail.com>
2023-10-05 10:29:17 +0300

    added form for adding books

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import AddBook from './AddBook';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+           
+            <li>
+              <Link to="/add-book">Add Books</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Routes>
+         
+          <Route path="/add-book" element={<AddBook />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
