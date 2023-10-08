fcd4da9f53a4a0a76baa6b0d2964b7ea6cf9d7da
Abed Nganga <ngangaabed@gmail.com>
2023-10-08 23:22:08 +0300

    Updated categorylist

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -1,105 +1,13 @@
-// import { useState, useEffect } from 'react';
-// import api from './Api';
-// import { Link } from 'react-router-dom';
-
-// function CategoryList() {
-//   const [categories, setCategories] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch the list of categories from the Flask API
-//     api.get('/categories')
-//       .then(response => {
-//         setCategories(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching categories:', error);
-//       });
-//   }, []);
-
-//   return (
-
-
-//     <div className="container">
-//       <h2 className="mt-4">Categories Library</h2>
-//       <ul className="list-group">
-        
-//         {categories.map(category => (
-//           <li key={category.id} className="list-group-item">
-//             <Link to={`/category/${category.id}`} className="text-decoration-none text-dark">
-//               {category.name}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default CategoryList;
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-
-// const BookList = ({ books }) => {
-//   return (
-//     <ul>
-//       {books.map((book) => (
-//         <li key={book.id}>{book.title}</li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// const CategoryDisplay = ({ category_id }) => {
-//   const [books, setBooks] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     fetch(`http://127.0.0.1:5500/category/${category_id}/books`)
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         setBooks(data);
-//         setError(null); // Clear any previous errors
-//       })
-//       .catch((error) => {
-//         console.error('Fetch error:', error);
-//         setError(error.message); // Set the error message in state
-//       });
-//   }, [category_id]);
-
-//   return (
-//     <div>
-//       <h1>Category Books</h1>
-//       {error ? (
-//         <p>Error: {error}</p>
-//       ) : (
-//         <BookList books={books} />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CategoryDisplay;
-
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BookCategories = () => {
+const BookCategories = ({category_id}) => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     // Fetch categories and their books from your Flask API
-    axios.get('http://127.0.0.1:5000/category/${category_id}/books')
+    axios.get(`http://127.0.0.1:5000/category/${category_id}/books`)
       .then(response => {
         setCategories(response.data);
       })
@@ -112,7 +20,7 @@ const BookCategories = () => {
     <div className="container">
       <h1>Book Categories</h1>
       {categories.map(category => (
-        <div key={category.id} className="my-4">
+        <div key={category_id} className="my-4">
           <h2>{category.name}</h2>
           <div className="row">
             {category.books.map(book => (
